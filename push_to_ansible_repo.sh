468f08fd83ee3af69a39322b0c979d8a51e58187
ramalaxmibandi <125037363+ramalaxmibandi@users.noreply.github.com>
2023-11-20 15:40:33 +0000

    Update push_to_ansible_repo.sh

--- push_to_ansible_repo.sh
+++ push_to_ansible_repo.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-# Set Ansible repository path and credentials if needed
-ANSIBLE_REPO="git@github.com:ramalaxmibandi/ansible-pipeline.git"
-ANSIBLE_DIR="https://github.com/ramalaxmibandi/ansible-pipeline"
-
-# Generated files from Terraform
-TERRAFORM_FILES="https://github.com/ramalaxmibandi/terraform-pipeline"
-
-# Navigate to the Ansible repository directory
-
-# Pull the latest changes in the Ansible repository
-git pull
-
 # Copy generated Ansible files from Terraform output
 cp "$TERRAFORM_FILES/ansible_inventory" .
 cp "$TERRAFORM_FILES/ansible.cfg" .
@@ -20,4 +8,5 @@ cp "$TERRAFORM_FILES/private_key.pem" .
 # Add, commit, and push changes
 git add ansible_inventory ansible.cfg private_key.pem
 git commit -m "Update Ansible files from Terraform"
+git remote add origin https://github.com/ramalaxmibandi/ansible-pipeline.git
 git push origin main  
