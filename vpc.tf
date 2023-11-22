2df738bcf6175bbc1c5b902fdff66db5f1ede0cf
ramalaxmibandi <125037363+ramalaxmibandi@users.noreply.github.com>
2023-11-22 13:58:04 +0000

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,8 +1,8 @@
-resource "aws_vpc" "cluster_vpc" {
+resource "aws_vpc" "clustert_vpc" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
   tags = {
-    Name = "aws_vpc"
+    Name = "aws_vpc1"
   }
 }
