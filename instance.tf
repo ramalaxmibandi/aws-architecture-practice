6b3b840aea355747f63a32e83edf80c369093190
ramalaxmibandi <125037363+ramalaxmibandi@users.noreply.github.com>
2023-11-28 15:01:39 +0000

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "mastert" {
   ami           = "ami-0ff1c68c6e837b183"
   instance_type = "t2.micro"
   count         = 1
-  key_name          = "jenkins-key"
+  key_name          = "jenkinterra-key"
   tags = {
     Name = "mastert"
   }
@@ -13,7 +13,7 @@ resource "aws_instance" "workert" {
   ami           = "ami-0ff1c68c6e837b183"
   instance_type = "t2.micro"
   count         = 2
-  key_name          = "jenkins-key"
+  key_name          = "jenkinterra-key"
   tags = {
     Name = "workert-${count.index + 1}"
   }
